facaa7caacdc6680d3d4a8f7fc4e8ff19684293a
brodyedwardrichards <brody.richards@edu.sait.ca>
2024-04-02 22:00:08 -0600

    test 1

--- script.js
+++ script.js
@@ -17,8 +17,56 @@ async function getNews() {
     try {
       const response = await fetch(api_url);
       const data = await response.json();
-      return data;
+      return data.articles;
     } catch (error) {
         console.log(error);
     }
 }
+
+
+// Populate dropdown list with articles //
+
+async function renderOption1Dropdown() {
+    const select = document.getElementById("dropdown");
+    const articles = await getNews();
+    if (articles) {
+        articles.forEach((article) => {
+            const option = document.createElement("option");
+            option.textContent = article.title;
+            option.value = article.url;
+            select.appendChild(option);
+        });
+    }
+}
+
+
+
+// Attach Event Listener to the submit button //
+
+
+const submitButton = document.getElementById('submit-button');
+submitButton.addEventListener('click', submitButtonClickHandler);
+
+
+// Populate UL based on category selection - stuck here i think
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
